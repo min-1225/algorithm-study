eeaf51bc66a2cf9a6b4393b2249ad769920afd9b
이상민(LeeSangMin) <zujumk@gmail.com>
2026-06-30 18:21:18 +0900

    Update and rename solution.java to trial2.java

--- Programmers/Level_0/등차등비수열_다음숫자구하기/trial2.java
+++ Programmers/Level_0/등차등비수열_다음숫자구하기/trial2.java
@@ -8,11 +8,16 @@ class Solution {
         int c = common[1] / common[0];   /* 공비 */
         
         if (a == b) {
-                answer = common[common.length-1] + a;
-            }
-            else
-                answer = common[common.length-1] * c;
+            answer = common[common.length-1] + a;
+        }
+        else
+            answer = common[common.length-1] * c;
         
         return answer;
     }
 }
+
+/*
+변수 c의 경우 comon[0]이 0일 때 에러 발생
+-> 등차수열이 아닐 때에만 계산하는 방식으로 변경
+*/
